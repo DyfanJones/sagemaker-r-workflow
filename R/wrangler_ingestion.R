752daeb7bc7d4a2c585d74193f001585117f925f
DyfanJones <dyfan.jones@shopdirect.com>
2021-07-02 17:11:06 +0100

    feature: add data wrangler classes

---
+++ R/wrangler_ingestion.R
@@ -0,0 +1,170 @@
+# NOTE: This code has been modified from AWS Sagemaker Python:
+# https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/wrangler/ingestion.py
+
+#' @import R6
+#' @import R6sagemaker.common
+#' @import uuid
+
+#' @title generate data ingestion flow from s3 input
+#' @description Generate the data ingestion only flow from s3 input
+#' @param input_name (str): the name of the input to flow source node
+#' @param s3_uri (str): uri for the s3 input to flow source node
+#' @param s3_content_type (str): s3 input content type
+#' @param s3_has_header (bool): flag indicating the input has header or not
+#' @param operator_version: (str): the version of the operator
+#' @param schema: (typing.Dict): the schema for the data to be ingested
+#' @return dict (typing.Dict): A flow only conduct data ingestion with 1-1 mapping
+#'              output_name (str): The output name used to configure
+#'              `sagemaker.processing.FeatureStoreOutput`
+generate_data_ingestion_flow_from_s3_input = function(input_name,
+                                                      s3_uri,
+                                                      s3_content_type="csv",
+                                                      s3_has_header=FALSE,
+                                                      operator_version="0.1",
+                                                      schema=NULL){
+  stopifnot(is.character(input_name),
+            is.character(s3_uri),
+            is.character(s3_content_type),
+            is.logical(s3_has_header),
+            is.character(operator_version),
+            is.list(schema) || is.null(schema))
+
+  source_node = list(
+    "node_id"=uuid::UUIDgenerate(),
+    "type"="SOURCE",
+    "inputs"=list(),
+    "outputs"=list(list("name"="default")),
+    "operator"=sprintf("sagemaker.s3_source_%s",operator_version),
+    "parameters"=list(
+      "dataset_definition"=list(
+        "datasetSourceType"="S3",
+        "name"=input_name,
+        "s3ExecutionContext"=list(
+          "s3Uri"=s3_uri,
+          "s3ContentType"=s3_content_type,
+          "s3HasHeader"=s3_has_header)
+      )
+    )
+  )
+  output_node = .get_output_node(source_node[["node_id"]], operator_version, schema)
+
+  flow = list(
+    "metadata"=list("version"=1, "disable_limits"=FALSE),
+    "nodes"=list(source_node, output_node)
+  )
+  return(list(flow, sprintf('%s.default',output_node[["node_id"]])))
+}
+
+#' @title generate data ingestion flow from athena dataset definition
+#' @description Generate the data ingestion only flow from athena input
+#' @param input_name (str): the name of the input to flow source node
+#' @param athena_dataset_definition (AthenaDatasetDefinition): athena input to flow source node
+#' @param operator_version: (str): the version of the operator
+#' @param schema: (typing.Dict): the schema for the data to be ingested
+#' @return dict (typing.Dict): A flow only conduct data ingestion with 1-1 mapping
+#'              output_name (str): The output name used to configure
+#'              `sagemaker.processing.FeatureStoreOutput`
+generate_data_ingestion_flow_from_athena_dataset_definition = function(input_name,
+                                                                       athena_dataset_definition,
+                                                                       operator_version="0.1",
+                                                                       schema=NULL){
+  stopifnot(is.character(input_name),
+            inherits(athena_dataset_definition, "AthenaDatasetDefinition"),
+            is.character(operator_version),
+            is.list(schema) || is.null(schema))
+  source_node = list(
+    "node_id"=uuid::UUIDgenerate(),
+    "type"="SOURCE",
+    "inputs"=list(),
+    "outputs"=list(list("name"="default")),
+    "operator"=sprintf("sagemaker.athena_source_%s", operator_version),
+    "parameters"=list(
+      "dataset_definition"=list(
+        "datasetSourceType"="Athena",
+        "name"=input_name,
+        "catalogName"=athena_dataset_definition$catalog,
+        "databaseName"=athena_dataset_definition$database,
+        "queryString"=athena_dataset_definition$query_string,
+        "s3OutputLocation"=athena_dataset_definition$output_s3_uri,
+        "outputFormat"=athena_dataset_definition$output_format)
+    )
+  )
+  output_node = .get_output_node(source_node[["node_id"]], operator_version, schema)
+
+  flow = list(
+    "metadata"=list("version"=1, "disable_limits"=FALSE),
+    "nodes"=list(source_node, output_node)
+  )
+  return(list(flow, sprintf('%s.default',output_node[["node_id"]])))
+}
+
+#' @title generate data ingestion flow from redshift dataset definition
+#' @description Generate the data ingestion only flow from redshift input
+#' @param input_name (str): the name of the input to flow source node
+#' @param redshift_dataset_definition (RedshiftDatasetDefinition): redshift input to flow source node
+#' @param operator_version: (str): the version of the operator
+#' @param schema: (typing.Dict): the schema for the data to be ingested
+#' @return dict (typing.Dict): A flow only conduct data ingestion with 1-1 mapping
+#'              output_name (str): The output name used to configure
+#'              `sagemaker.processing.FeatureStoreOutput`
+generate_data_ingestion_flow_from_redshift_dataset_definition = function(input_name,
+                                                                         redshift_dataset_definition,
+                                                                         operator_version="0.1",
+                                                                         schema=NULL){
+  stopifnot(is.character(input_name),
+            inherits(athena_dataset_definition, "RedshiftDatasetDefinition"),
+            is.character(operator_version),
+            is.list(schema) || is.null(schema))
+  source_node = list(
+    "node_id"=uuid::UUIDgenerate(),
+    "type"="SOURCE",
+    "inputs"=list(),
+    "outputs"=list(list("name"="default")),
+    "operator"=sprintf("sagemaker.redshift_source_%s", operator_version),
+    "parameters"=list(
+      "dataset_definition"=list(
+        "datasetSourceType"="Redshift",
+        "name"=input_name,
+        "clusterIdentifier"=redshift_dataset_definition$cluster_id,
+        "database"=redshift_dataset_definition$database,
+        "dbUser"=redshift_dataset_definition$db_user,
+        "queryString"=redshift_dataset_definition$query_string,
+        "unloadIamRole"=redshift_dataset_definition$cluster_role_arn,
+        "s3OutputLocation"=redshift_dataset_definition$output_s3_uri,
+        "outputFormat"=redshift_dataset_definition$output_format)
+    )
+  )
+  output_node = .get_output_node(source_node[["node_id"]], operator_version, schema)
+
+  flow = list(
+    "metadata"=list("version"=1, "disable_limits"=FALSE),
+    "nodes"=list(source_node, output_node)
+  )
+  return(list(flow, sprintf('%s.default',output_node[["node_id"]])))
+}
+
+# A helper function to generate output node, for internal use only
+# Args:
+#   source_node_id (str): source node id
+# operator_version: (str): the version of the operator
+# schema: (typing.Dict): the schema for the data to be ingested
+# Returns:
+#   dict (typing.Dict): output node
+.get_output_node = function(source_node_id,
+                            operator_version,
+                            schema){
+  stopifnot(is.character(source_node_id),
+            is.character(operator_version),
+            is.list(schema) || is.null(schema))
+
+  return(list(
+    "node_id"=uuid::UUIDgenerate(),
+    "type"="TRANSFORM",
+    "operator"=sprintf("sagemaker.spark.infer_and_cast_type_%s", operator_version),
+    "trained_parameters"= schema %||% list(),
+    "parameters"=list(),
+    "inputs"=list(list("name"="default", "node_id"=source_node_id, "output_name"="default")),
+    "outputs"=list(list("name"="default"))
+    )
+  )
+}
---
+++ R/wrangler_processing.R
@@ -0,0 +1,146 @@
+# NOTE: This code has been modified from AWS Sagemaker Python:
+# https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/wrangler/ingestion.py
+
+#' @import R6
+#' @import R6sagemaker.common
+#' @import uuid
+
+#' @title DataWranglerProcessor class
+#' @description Handles Amazon SageMaker DataWrangler tasks
+#' @export
+DataWranglerProcessor = R6Class(
+  inherit = R6sagemaker.common::Processor,
+  public = list(
+
+    #' @description Initializes a ``Processor`` instance.
+    #'              The ``Processor`` handles Amazon SageMaker Processing tasks.
+    #' @param role (str): An AWS IAM role name or ARN. Amazon SageMaker Processing
+    #'              uses this role to access AWS resources, such as
+    #'              data stored in Amazon S3.
+    #' @param data_wrangler_flow_source (str): The source of the DaraWrangler flow which will be
+    #'              used for the DataWrangler job. If a local path is provided, it will automatically
+    #'              be uploaded to S3 under:
+    #'              "s3://<default-bucket-name>/<job-name>/input/<input-name>".
+    #' @param instance_count (int): The number of instances to run
+    #'              a processing job with.
+    #' @param instance_type (str): The type of EC2 instance to use for
+    #'              processing, for example, 'ml.c4.xlarge'.
+    #' @param volume_size_in_gb (int): Size in GB of the EBS volume
+    #'              to use for storing data during processing (default: 30).
+    #' @param volume_kms_key (str): A KMS key for the processing
+    #'              volume (default: None).
+    #' @param output_kms_key (str): The KMS key ID for processing job outputs (default: None).
+    #' @param max_runtime_in_seconds (int): Timeout in seconds (default: None).
+    #'              After this amount of time, Amazon SageMaker terminates the job,
+    #'              regardless of its current status. If `max_runtime_in_seconds` is not
+    #'              specified, the default value is 24 hours.
+    #' @param base_job_name (str): Prefix for processing job name. If not specified,
+    #'              the processor generates a default job name, based on the
+    #'              processing image name and current timestamp.
+    #' @param sagemaker_session (:class:`~sagemaker.session.Session`):
+    #'              Session object which manages interactions with Amazon SageMaker and
+    #'              any other AWS services needed. If not specified, the processor creates
+    #'              one using the default AWS configuration chain.
+    #' @param env (dict[str, str]): Environment variables to be passed to
+    #'              the processing jobs (default: None).
+    #' @param tags (list[dict]): List of tags to be passed to the processing job
+    #'              (default: None). For more, see
+    #'              \url{https://docs.aws.amazon.com/sagemaker/latest/dg/API_Tag.html}.
+    #' @param network_config (:class:`~sagemaker.network.NetworkConfig`):
+    #'              A :class:`~sagemaker.network.NetworkConfig`
+    #'              object that configures network isolation, encryption of
+    #'              inter-container traffic, security group IDs, and subnets.
+    initialize = function(role,
+                          data_wrangler_flow_source,
+                          instance_count,
+                          instance_type,
+                          volume_size_in_gb=30L,
+                          volume_kms_key=NULL,
+                          output_kms_key=NULL,
+                          max_runtime_in_seconds=NULL,
+                          base_job_name=NULL,
+                          sagemaker_session=NULL,
+                          env=NULL,
+                          tags=NULL,
+                          network_config=NULL){
+      stopifnot(is.character(role),
+                is.character(data_wrangler_flow_source),
+                is.integer(instance_count) || is.numeric(instance_count),
+                is.character(instance_type),
+                is.integer(volume_size_in_gb) || is.numeric(volume_size_in_gb),
+                is.character(volume_kms_key) || is.null(volume_kms_key),
+                is.character(output_kms_key) || is.null(output_kms_key),
+                is.character(max_runtime_in_seconds) || is.numeric(max_runtime_in_seconds) || is.null(max_runtime_in_seconds),
+                inherits(sagemaker_session, "Session") || is.null(sagemaker_session),
+                is.list(env) || is.null(env),
+                is.list(tags) || is.null(tags),
+                inherits(network_config, "NetworkConfig") || is.null(network_config)
+      )
+
+      self$data_wrangler_flow_source = data_wrangler_flow_source
+      self$sagemaker_session = sagemaker_session %||% R6sagemaker.common::Session$new()
+      image_uri = R6sagemaker.common::ImageUris$new()$retrieve(
+        "data-wrangler", region=self$sagemaker_session$paws_region_name
+      )
+      super$intialize(
+        role,
+        image_uri,
+        instance_count,
+        instance_type,
+        volume_size_in_gb=volume_size_in_gb,
+        volume_kms_key=volume_kms_key,
+        output_kms_key=output_kms_key,
+        max_runtime_in_seconds=max_runtime_in_seconds,
+        base_job_name=base_job_name,
+        sagemaker_session=sagemaker_session,
+        env=env,
+        tags=tags,
+        network_config=network_config
+      )
+    }
+  ),
+  private = list(
+
+    # Normalizes the arguments so that they can be passed to the job run
+    # Args:
+    #   job_name (str): Name of the processing job to be created. If not specified, one
+    # is generated, using the base name given to the constructor, if applicable
+    # (default: None).
+    # arguments (list[str]): A list of string arguments to be passed to a
+    # processing job (default: None).
+    # inputs (list[:class:`~sagemaker.processing.ProcessingInput`]): Input files for
+    # the processing job. These must be provided as
+    # :class:`~sagemaker.processing.ProcessingInput` objects (default: None).
+    # outputs (list[:class:`~sagemaker.processing.ProcessingOutput`]): Outputs for
+    # the processing job. These can be specified as either path strings or
+    # :class:`~sagemaker.processing.ProcessingOutput` objects (default: None).
+    # code (str): This can be an S3 URI or a local path to a file with the framework
+    # script to run (default: None). A no op in the base class.
+    # kms_key (str): The ARN of the KMS key that is used to encrypt the
+    # user code file (default: None).
+    .normalize_args = function(job_name=NULL,
+                               arguments=NULL,
+                               inputs=NULL,
+                               outputs=NULL,
+                               code=NULL,
+                               kms_key=NULL){
+      inputs = inputs %||% list()
+      found = any(sapply(inputs, function(element) element$input_name == "flow"))
+      if (!found)
+        inputs = c(inputs, private$.get_recipe_input())
+      return (super$.normalize_args(job_name, arguments, inputs, outputs, code, kms_key))
+    },
+
+    .get_recipe_input = function(){
+      return(ProcessingInput$new(
+        source=self$data_wrangler_flow_source,
+        destination="/opt/ml/processing/flow",
+        input_name="flow",
+        s3_data_type="S3Prefix",
+        s3_input_mode="File",
+        s3_data_distribution_type="FullyReplicated")
+      )
+    }
+  ),
+  lock_objects=F
+)
