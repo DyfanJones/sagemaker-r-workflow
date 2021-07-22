19920bd309a82a58d518734f2c4671ae32455123
DyfanJones <dyfan.jones@shopdirect.com>
2021-07-22 17:42:12 +0100

    feature: initial execution variables

---
+++ R/workflow_execution_variables.R
@@ -0,0 +1,71 @@
+# NOTE: This code has been modified from AWS Sagemaker Python:
+# https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/workflow/execution_variables.py
+
+#' @include workflow_entities.R
+
+#' @import R6
+#' @import R6sagemaker.common
+
+#' @title Workflow ExecutionVariable class
+#' @description Pipeline execution variables for workflow.
+#' @export
+ExecutionVariable = R6Class("ExecutionVariable",
+  inherit = Expression,
+  public = list(
+
+    #' @field name
+    #' The name of the execution variable.
+    name = NULL,
+
+    #' @description Create a pipeline execution variable.
+    #' @param name (str): The name of the execution variable.
+    initialize = function(name){
+      stopifnot(
+        is.character(name)
+      )
+      self$name = name
+    }
+  ),
+  active =list(
+
+    #' @field expr
+    #' The 'Get' expression dict for an `ExecutionVariable`.
+    expr = function(){
+      return(list("Get"=sprintf("Execution.%s", self$name)))
+    }
+  )
+)
+
+#' @title Enum-like class for all ExecutionVariable instances.
+#' @description Considerations to move these as module-level constants should be made.
+#' @export
+ExecutionVariables = R6Class("ExecutionVariables",
+  public = list(
+
+
+    #' @field START_DATETIME
+    #'
+    START_DATETIME = ExecutionVariable$new("StartDateTime"),
+
+    #' @field CURRENT_DATETIME
+    #'
+    CURRENT_DATETIME = ExecutionVariable$new("CurrentDateTime"),
+
+    #' @field PIPELINE_NAME
+    #'
+    PIPELINE_NAME = ExecutionVariable$new("PipelineName"),
+
+    #' @field PIPELINE_ARN
+    #'
+    PIPELINE_ARN = ExecutionVariable$new("PipelineArn"),
+
+    #' @field PIPELINE_EXECUTION_ARN
+    #'
+    PIPELINE_EXECUTION_ARN = ExecutionVariable$new("PipelineExecutionArn"),
+
+    #' @description format class
+    format = function(){
+      format_class(self)
+    }
+  )
+)
