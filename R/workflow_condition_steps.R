0c4efe279b52755571569ad77f6b56028b40eb90
DyfanJones <dyfan.jones@shopdirect.com>
2021-07-22 17:39:41 +0100

    feature: placeholder files for conditions/condition step

---
+++ R/workflow_condition_steps.R
@@ -0,0 +1,14 @@
+# NOTE: This code has been modified from AWS Sagemaker Python:
+# https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/workflow/condition_step.py
+
+#' @include workflow_entities.R
+#' @include workflow_properties.R
+#' @include workflow_steps.R
+#' @include r_utils.R
+
+#' @import R6
+#' @import R6sagemaker.common
+#' @import R6sagemaker.mlcore
+
+
+
---
+++ R/workflow_contidions.R
@@ -0,0 +1,15 @@
+# NOTE: This code has been modified from AWS Sagemaker Python:
+# https://github.com/aws/sagemaker-python-sdk/blob/master/src/sagemaker/workflow/conditions.py
+
+#' @include workflow_entities.R
+#' @include workflow_execution_variables.R
+#' @include workflow_parameters.R
+#' @include workflow_properties.R
+#' @include r_utils.R
+
+#' @import R6
+#' @import R6sagemaker.common
+#' @import R6sagemaker.mlcore
+
+
+
