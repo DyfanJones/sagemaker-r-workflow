aee485dc758eb4eee8efe5667276c1f3ad2b4ac4
Dyfan Jones <dyfan.jones@theverygroup.com>
2022-02-03 14:19:31 +0000

    feature: helper functions

--- R/r_utils.R
+++ R/r_utils.R
@@ -1,3 +1,20 @@
-`%||%` <- function(x, y) if (is.null(x)) return(y) else return(x)
+`%||%` = function(x, y) if (is.null(x)) return(y) else return(x)
 
 enum_items = function(enum) unlist(as.list(enum), use.names = F)
+
+list.append = function(.data, ...) {
+  if (is.list(.data)) c(.data, list(...)) else c(.data, ..., recursive = FALSE)
+}
+
+# Sets dictionary[key] = value if value is not None.
+.set = function(value, key, dictionary){
+  if (!is.null(value)) {
+    temp_dict = deparse(substitute(dictionary))
+    dictionary[[key]] = value
+    assign(temp_dict, dictionary, envir = parent.frame())
+  }
+}
+
+paws_error_code = function(error){
+  return(error[["error_response"]][["__type"]] %||% error[["error_response"]][["Code"]])
+}
